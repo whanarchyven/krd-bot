d32f64ddff29c10f6d6e513ea5ac84bea27389f6
Ivan Zaburdaev <ivanlom227@gmail.com>
2023-11-23 22:58:59 +0300

    demo

---
+++ index.js
@@ -0,0 +1,20 @@
+const TelegramApi=require('node-telegram-bot-api')
+
+const token='6384276235:AAEGyfBmhCSgizgLa3_vRbZ1VSFcPtYZAHk'
+
+const bot= new TelegramApi(token,{polling:true})
+
+bot.setMyCommands([
+    {command:'/start',description:'Привязать бота к чату'}
+])
+
+bot.on('message',async (msg)=>{
+    const text=msg.text;
+    const chatId=msg.chat.id
+    console.log(chatId,text)
+
+    if(text=='/start'){
+        bot.sendMessage(chatId,`Код привязки бота: <b>${chatId}</b>`,{parse_mode:'HTML'})
+        bot.setChatPhoto(chatId,'images/some.jpg')
+    }
+})
